3dcb91265eea323c70ebc62de909dbf20a3bdbd9
Flavio Esteves <flavioafesteves@gmail.com>
2023-10-20 17:15:26 +0100

    UPD: CI/CD

--- tests/api/exercise.rs
+++ tests/api/exercise.rs
@@ -19,19 +19,3 @@ async fn get_all_exercises_status_200() {
     // Assert
     assert_eq!(response.status(), 200);
 }
-
-#[tokio::test]
-async fn crud_exercise_status_200() {
-    // Arrange
-    let app = spawn_app().await;
-
-    //Act
-    let response = app
-        .api_client
-        .post(&format!("{}/exercise", &app.address))
-        .send()
-        .await
-        .expect("Failed to add a new exercise POST request.");
-
-    println!("Response {:?}", response);
-}
